e413f03b259a003dac7c03d4a726512ffea89cf9
harveybrezinaconniffe <harvey@conniffe.com>
2024-01-02 00:27:17 +0000

    Week2Assignement/week2.csv

--- Week2Assignement/week2.py
+++ Week2Assignement/week2.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.dummy import DummyClassifier
 
 # Load data
 df = pd.read_csv("week2.csv", header=None)
@@ -45,7 +46,7 @@ print("Intercept: {}".format(logistic_classifier.intercept_[0]))
 # (a)(iii)
 # Generate predictions
 
-def plot_classifier(pred, plot, decision_boundary_xs, decision_boundary_ys, legend=True):
+def plot_classifier(pred, plot, decision_boundary_xs=None, decision_boundary_ys=None, legend=True):
   # Plot positive train values
   plot.plot(X[Y == 1][:, 0], 
            X[Y == 1][:, 1],
@@ -77,7 +78,8 @@ def plot_classifier(pred, plot, decision_boundary_xs, decision_boundary_ys, lege
            markerfacecolor="none",
            label="-1 Predicted")
 
-  plot.plot(decision_boundary_xs, decision_boundary_ys)
+  if decision_boundary_xs != None:
+    plot.plot(decision_boundary_xs, decision_boundary_ys)
   plot.set_xlabel("x_1")
   plot.set_ylabel("x_2")
   if legend:
@@ -94,7 +96,7 @@ def calculate_linear_decision_boundary(classifier):
 xs, ys = calculate_linear_decision_boundary(logistic_classifier)
 preds = logistic_classifier.predict(X)
 fig, ax = plt.subplots()
-plot_classifier(preds, ax, xs, ys)
+plot_classifier(preds, ax, decision_boundary_xs=xs, decision_boundary_ys=ys)
 
 acc = accuracy_score(Y, preds)
 print("Train accuracy: {}".format(acc))
@@ -140,4 +142,38 @@ plt.show()
 # (C)
 data = np.insert(data, 2, data[:, 0]**2, axis=1)
 data = np.insert(data, 3, data[:, 1]**2, axis=1)
+X = data[:, :4]
+Y = data[:, 4]
 
+logistic_classifier = LogisticRegression(penalty=None, random_state=0).fit(
+  X, 
+  Y)
+
+print("Logistic regression parameters:")
+print("Parameters: {}".format(logistic_classifier.coef_[0]))
+print("Intercept: {}".format(logistic_classifier.intercept_[0]))
+
+#def calculate_quadratic_decision_boundary(classifier):
+#  x1s = np.linspace(-1, 1, 1000)
+#  y = x**2 + 2*x + 2
+
+#xs, ys = calculate_quadratic_decision_boundary(logistic_classifier)
+preds = logistic_classifier.predict(X)
+fig, ax = plt.subplots()
+plot_classifier(preds, ax)
+
+acc = accuracy_score(Y, preds)
+print("Train accuracy: {}".format(acc))
+print(classification_report(Y, preds))
+print()
+
+most_frequent_classifier = DummyClassifier(strategy="most_frequent")
+most_frequent_classifier.fit(X, Y)
+
+preds = most_frequent_classifier.predict(X)
+acc = accuracy_score(Y, preds)
+
+print("Most Frequent Train accuracy: {}".format(acc))
+print(classification_report(Y, preds))
+print()
+plt.show()
